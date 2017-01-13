59d0643aadae8c5fcc8c52ca033bfef5f3c3afe5
Claudia Rosas (BSC) <crosas@bsc.es>
2017-01-13 11:57:13 +0100

    Changing structure

--- 1_OpmSs_version/trace.ompss.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-module load extrae
-export EXTRAE_CONFIG_FILE=./extrae_conf/extrae.ompss.xml
-export NX_ARGS="${NX_ARGS} --instrumentation=extrae "
-export LD_PRELOAD=${EXTRAE_HOME}/lib/libnanosmpitrace.so
-#export LD_PRELOAD=${EXTRAE_HOME}/lib/libnanosmpitracef.so
-
-$*
